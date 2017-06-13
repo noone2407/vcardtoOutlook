ebea11e9a8d6c29145f7265c0ae2ece9a6f57eff
Dung Le <xdung24@gmail.com>
2017-06-13 13:02:44 +0700

    First commit

---
+++ VcardToOutlook/MainWindow.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace VcardToOutlook
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.buttonCut = new System.Windows.Forms.Button();
+            this.buttonSelectTarget = new System.Windows.Forms.Button();
+            this.buttonSelectSource = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClearFolder = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "VCardToOutlook 1.0";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(225, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 47);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Select a VCF file and then click on \'Cut\' to split it into individual VCF files. " +
+    "Then you can copy them to your phone.";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(222, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 32);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "You can also import to outlook with one-click.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonClearFolder);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxOutput);
+            this.groupBox1.Controls.Add(this.textBoxInput);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.buttonSelectTarget);
+            this.groupBox1.Controls.Add(this.buttonSelectSource);
+            this.groupBox1.Location = new System.Drawing.Point(22, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(389, 116);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Provide details";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output Folder";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(19, 81);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(306, 20);
+            this.textBoxOutput.TabIndex = 5;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(19, 33);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(334, 20);
+            this.textBoxInput.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Source (Original vCard File)";
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(103, 264);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(70, 35);
+            this.buttonImport.TabIndex = 8;
+            this.buttonImport.Text = "Import";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.Location = new System.Drawing.Point(341, 264);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(70, 35);
+            this.buttonAbout.TabIndex = 9;
+            this.buttonAbout.Text = "About";
+            this.buttonAbout.UseVisualStyleBackColor = true;
+            this.buttonAbout.Click += new System.EventHandler(this.buttonAbout_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(180, 276);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(155, 23);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 10;
+            this.progressBar.Visible = false;
+            // 
+            // buttonCut
+            // 
+            this.buttonCut.Location = new System.Drawing.Point(22, 264);
+            this.buttonCut.Name = "buttonCut";
+            this.buttonCut.Size = new System.Drawing.Size(70, 35);
+            this.buttonCut.TabIndex = 7;
+            this.buttonCut.Text = "Cut";
+            this.buttonCut.UseVisualStyleBackColor = true;
+            this.buttonCut.Click += new System.EventHandler(this.buttonCut_Click);
+            // 
+            // buttonSelectTarget
+            // 
+            this.buttonSelectTarget.Image = global::VcardToOutlook.Properties.Resources.output;
+            this.buttonSelectTarget.Location = new System.Drawing.Point(331, 78);
+            this.buttonSelectTarget.Name = "buttonSelectTarget";
+            this.buttonSelectTarget.Size = new System.Drawing.Size(24, 23);
+            this.buttonSelectTarget.TabIndex = 4;
+            this.buttonSelectTarget.UseVisualStyleBackColor = true;
+            this.buttonSelectTarget.Click += new System.EventHandler(this.buttonSelectTarget_Click);
+            // 
+            // buttonSelectSource
+            // 
+            this.buttonSelectSource.Image = global::VcardToOutlook.Properties.Resources.open;
+            this.buttonSelectSource.Location = new System.Drawing.Point(359, 30);
+            this.buttonSelectSource.Name = "buttonSelectSource";
+            this.buttonSelectSource.Size = new System.Drawing.Size(24, 23);
+            this.buttonSelectSource.TabIndex = 0;
+            this.buttonSelectSource.UseVisualStyleBackColor = true;
+            this.buttonSelectSource.Click += new System.EventHandler(this.buttonSelectSource_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VcardToOutlook.Properties.Resources.Microsoft_outlook;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(193, 103);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonClearFolder
+            // 
+            this.buttonClearFolder.Image = global::VcardToOutlook.Properties.Resources.delete;
+            this.buttonClearFolder.Location = new System.Drawing.Point(361, 78);
+            this.buttonClearFolder.Name = "buttonClearFolder";
+            this.buttonClearFolder.Size = new System.Drawing.Size(24, 23);
+            this.buttonClearFolder.TabIndex = 7;
+            this.buttonClearFolder.UseVisualStyleBackColor = true;
+            this.buttonClearFolder.Click += new System.EventHandler(this.buttonClearFolder_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 303);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.buttonImport);
+            this.Controls.Add(this.buttonCut);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VCard To OutLook";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSelectSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonSelectTarget;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonCut;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button buttonClearFolder;
+    }
+}
+
---
+++ VcardToOutlook/MainWindow.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+namespace VcardToOutlook
+{
+    public partial class MainWindow : Form
+    {
+        Outlook.Application App;
+        string ContactFolder = "C:\\Contacts\\";
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = ContactFolder;
+            if (!Directory.Exists(ContactFolder))
+                Directory.CreateDirectory(ContactFolder);
+        }
+
+        private void buttonSelectSource_Click(object sender, EventArgs e)
+        {
+            var ofdSource = new OpenFileDialog();
+            ofdSource.Filter = "Vcf file|*.vcf";
+            if (ofdSource.ShowDialog() == DialogResult.OK)
+            {
+                textBoxInput.Text = ofdSource.FileName;
+            }
+        }
+
+        private void buttonSelectTarget_Click(object sender, EventArgs e)
+        {
+            var fbdTarget = new FolderBrowserDialog();
+            if (fbdTarget.ShowDialog() == DialogResult.OK)
+            {
+                textBoxOutput.Text = fbdTarget.SelectedPath;
+            }
+        }
+
+        private void buttonClearFolder_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(ContactFolder);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (Exception) { }
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                try
+                {
+                    dir.Delete(true);
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void buttonCut_Click(object sender, EventArgs e)
+        {
+            string inputFile = textBoxInput.Text;
+            string OutputFolder = textBoxOutput.Text;
+
+            if (!File.Exists(inputFile)) return;
+            if (!Directory.Exists(OutputFolder)) return;
+
+
+            string textData = string.Empty;
+            string name = string.Empty;
+            string filename = string.Empty;
+            bool flabegin = false;
+            bool flagend = false;
+            int counter = 0;
+
+            string[] allLines = File.ReadAllLines(inputFile);
+            progressBar.Visible = true;
+            progressBar.Value = 0;
+            progressBar.Maximum = allLines.Length;
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                string text4 = allLines[i];
+                if (text4 == "BEGIN:VCARD")
+                {
+                    flabegin = true;
+                }
+                if (text4 == "END:VCARD")
+                {
+                    flagend = true;
+                }
+                if (text4.StartsWith("N:"))
+                {
+                    text4 = "N;CHARSET=utf-8:" + text4.Substring(2);
+                }
+                if (text4.StartsWith("FN:"))
+                {
+                    name = text4.Substring(3);
+                    name = CleanFileName(name);
+                    text4 = "FN;CHARSET=utf-8:" + text4.Substring(3);
+                }
+                if (flabegin)
+                {
+                    textData = textData + Environment.NewLine + text4;
+                }
+                if (flagend)
+                {
+                    if (string.IsNullOrWhiteSpace(name))
+                    {
+                        int noNameCount = Directory.GetFiles(OutputFolder, "Noname_*", SearchOption.TopDirectoryOnly).Length;
+                        name = "Noname_" + noNameCount.ToString();
+                    }
+                    filename = name + ".vcf";
+                    string filePath = Path.Combine(OutputFolder, filename);
+                    File.WriteAllText(filePath, textData);
+                    flabegin = false;
+                    flagend = false;
+                    textData = string.Empty;
+                    name = string.Empty;
+                    filename = string.Empty;
+                    counter++;
+                }
+            }
+            MessageBox.Show("Your VCard was split into " + counter.ToString() + " files.", "Success", MessageBoxButtons.OK);
+            progressBar.Visible = false;
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            string OutputFolder = textBoxOutput.Text;
+            App = new Outlook.Application();
+            string folderPath = App.Session.DefaultStore.GetRootFolder().FolderPath + @"\Contacts\Key Contacts";
+            Outlook.Folder folder = GetFolder(folderPath);
+            ImportContacts(OutputFolder, folder);
+            App.Quit();
+        }
+
+        private void buttonAbout_Click(object sender, EventArgs e)
+        {
+            var about = new About();
+            about.ShowDialog();
+        }
+
+        private void ImportContacts(string path, Outlook.Folder targetFolder)
+        {
+            Outlook.ContactItem contact;
+            progressBar.Visible = true;
+            progressBar.Value = 0;
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path, "*.vcf");
+                progressBar.Maximum = files.Length;
+                int counter = 0;
+                foreach (string file in files)
+                {
+                    contact = App.Session.OpenSharedItem(file) as Outlook.ContactItem;
+                    contact.Save();
+                    counter++;
+                }
+                MessageBox.Show(string.Format("Imported {0}contact(s) to outlook.", counter.ToString()), "Success", MessageBoxButtons.OK);
+                progressBar.Visible = false;
+            }
+        }
+        private Outlook.Folder GetFolder(string folderPath)
+        {
+            Outlook.Folder folder;
+            string backslash = @"\";
+            try
+            {
+                if (folderPath.StartsWith(@"\\"))
+                {
+                    folderPath = folderPath.Remove(0, 2);
+                }
+                String[] folders = folderPath.Split(backslash.ToCharArray());
+                folder = App.Session.Folders[folders[0]] as Outlook.Folder;
+                if (folder != null)
+                {
+                    for (int i = 1; i <= folders.GetUpperBound(0); i++)
+                    {
+                        Outlook.Folders subFolders = folder.Folders;
+                        folder = subFolders[folders[i]] as Outlook.Folder;
+                        if (folder == null)
+                        {
+                            return null;
+                        }
+                    }
+                }
+                return folder;
+            }
+            catch { return null; }
+        }
+        private static string CleanFileName(string fileName)
+        {
+            string newFileName = fileName;
+            var invalidChars = Path.GetInvalidFileNameChars();
+            foreach (char c in invalidChars)
+            {
+                newFileName = newFileName.Replace(c.ToString(), "");
+            }
+            return newFileName;
+        }
+
+  
+    
+    }
+}
