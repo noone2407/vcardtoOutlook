5c928f53e119df74e5900bbdd5d938a41d3468dc
Dung Le (SM) <dungxle@deloitte.ca>
2021-10-11 23:41:14 +0700

    Check mandantory when updating

--- VcardToOutlook/MainWindow.cs
+++ VcardToOutlook/MainWindow.cs
@@ -144,20 +144,26 @@ namespace VcardToOutlook
                 return;
             if (!(checkUpdateresult.Version > appVersion))
                 return;
-            if (MessageBox.Show($"Do you want to update to version {checkUpdateresult.Version.ToString()}?", "There is a new version!", MessageBoxButtons.YesNo) == DialogResult.No)
-                return;
+            if (!checkUpdateresult.Mandatory)
+            {
+                if (MessageBox.Show($"Do you want to update to version {checkUpdateresult.Version.ToString()}?", "There is a new version!", MessageBoxButtons.YesNo) == DialogResult.No)
+                    return;
+            }
             var downloadUpdateResult = autoUpdate.DownloadUpdate(checkUpdateresult.Url);
             if (!downloadUpdateResult.Success)
             {
-                MessageBox.Show("Couldn't download the update", "Error");
+                MessageBox.Show("Couldn't download the update", "Error", MessageBoxButtons.OK);
                 return;
             }
-            if (MessageBox.Show("Do you want to restart now?", "Restart", MessageBoxButtons.YesNo) == DialogResult.No)
-                return;
+            if (!checkUpdateresult.Mandatory)
+            {
+                if (MessageBox.Show("Do you want to restart now?", "Restart", MessageBoxButtons.YesNo) == DialogResult.No)
+                    return;
+            }
             string extractPath = autoUpdate.UnzipUpdate(downloadUpdateResult.DownloadPath);
             if (string.IsNullOrEmpty(extractPath))
                 return;
-            string scriptPath = autoUpdate.CreateUpdateScript(extractPath,Application.StartupPath,Application.ExecutablePath,1);
+            string scriptPath = autoUpdate.CreateUpdateScript(extractPath, Application.StartupPath, Application.ExecutablePath, 1);
             if (!File.Exists(scriptPath))
                 return;
             Process.Start(scriptPath);
